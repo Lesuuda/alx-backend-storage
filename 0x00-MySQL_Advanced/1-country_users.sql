94755300eabe62ded362802dde1d18a09f4f8d5d
lesuuda <lesuudaljamani@gmail.com>
2024-07-11 10:00:26 +0300

    fix for number 1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,4 @@
 -- creates a table users with some attributes
-DROP TABLE users;
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
