a47de0df999cc793910b98548d2904a7ae9a9d4b
Lesuuda <lesuudaljamani@gmail.com>
2024-07-16 02:06:14 -0700

    fix for list documents uding pyhon

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -6,8 +6,7 @@ lists all documents in a colection
 
 def list_all(mongo_collection):
     """lists all documents in a collection"""
-    doc_list = [doc for docs in mongo_collection.find()]
-    if len(doc_list) < 1:
+    if mongo_collection is None:
         return []
-    return doc_list
+    return list(mongo_collection.find())
 
