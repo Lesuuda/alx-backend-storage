e6c88c6fd25bba07dc24c477c1308325f18a638d
lesuuda <lesuudaljamani@gmail.com>
2024-07-17 15:13:09 +0300

    core: writing strings to Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+writes strings to redis
+"""
+
+
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """
+    Cache class
+    """
+    def __init__(self):
+        """ init method"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ strore data in redis"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return (key)
